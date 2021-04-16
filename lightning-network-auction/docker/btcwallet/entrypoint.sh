aefb6dfe95fd709a44d510955a4efdc0b40bc726
regata <renat.g@gmail.com>
2021-04-15 22:01:55 -0400

    remove obsolete file

--- lightning-network-auction/docker/btcwallet/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-
-
-btcwallet --simnet \
-    --appdata=/data \
-    --cafile=/btcd-rpc/rpc.cert \
-    --rpcconnect=btcd:${BTCD_RPCPORT} \
-    --username=${BTCD_RPCUSER} \
-    --password=${BTCD_RPCPASS} \
-    --createtemp
