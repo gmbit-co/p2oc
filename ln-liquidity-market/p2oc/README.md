84a24f8757bde96ac66bb2f80b6accc3cd5a3517
Julian Villella <julian.villella@hotmail.com>
2021-05-01 00:04:24 -0400

    Add python package scaffolding for p2oc

---
+++ ln-liquidity-market/p2oc/README.md
@@ -0,0 +1,5 @@
+# `p2oc` (aka Pay-To-Open-Channel)
+
+![Code style](https://img.shields.io/badge/code%20style-black-000000.svg)
+
+...
---
+++ ln-liquidity-market/p2oc/setup.py
@@ -0,0 +1,31 @@
+from os import path
+from setuptools import setup, find_packages
+
+this_dir = path.abspath(path.dirname(__file__))
+with open(path.join(this_dir, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="p2oc",
+    description="Pay to open LN channel CLI tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="0.1",
+    packages=find_packages(exclude="tests"),
+    include_package_data=True,
+    zip_safe=True,
+    python_requires=">=3.7",
+    install_requires=[
+        "grpcio",
+        "click"
+    ],
+    extras_require={
+        "test": [
+            "pytest",
+        ],
+        "dev": [
+            "black",
+        ],
+    },
+    entry_points={"console_scripts": ["p2oc=p2oc:main"]},
+)
